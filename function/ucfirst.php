828a9ab405c3b5df4baae7726d6ae69b0196c5ae
magva7 <magva7@gmail.com>
2023-08-10 22:54:49 +0600

    практика

---
+++ function/ucfirst.php
@@ -0,0 +1,10 @@
+<?php
+/*Напишите программу, которая использует функцию ucfirst() с переменной $text и 
+выводит результат на экран. Значение переменной $text уже определено.*/
+
+$text = 'mount';
+$result = ucfirst($text);
+print_r($result);
+// BEGIN (write your solution here)
+
+// END
---
+++ string/print_r.php
@@ -0,0 +1,14 @@
+<?php
+
+$text = 'Never forget what you are, for surely the world will not';
+
+// BEGIN (write your solution here)
+print_r("First: " . $text[0] . "\n" . "Last: " . $text[-1]);
+// END
+
+/*Выведите на экран первую и последнюю буквы предложения, записанного в переменную $text,
+ в следующем формате:
+
+First: N
+Last: t
+Постарайтесь создать только одну переменную, в которую сразу запишется нужный текст*/
