f6f1e3f522025bbb25154b928da0c7a35fa941fe
magva7 <magva7@gmail.com>
2023-08-19 01:23:21 +0600

    Подключение внешних файлов

---
+++ connect_external_file/src/Strings.php
@@ -0,0 +1,16 @@
+<?php
+
+function reverse($str)
+{
+    $index = strlen($str) - 1;
+    $reversedString = '';
+
+    while ($index >= 0) {
+        $currentChar = $str[$index];
+        $reversedString = "{$reversedString}{$currentChar}";
+        $index = $index - 1;
+    }
+
+    return $reversedString;
+}
+// print_r("Тестовый вывод - файл Strings.php успешно подключен");
--- connect_external_file/src/connect_external_file.php
+++ connect_external_file/src/connect_external_file.php
@@ -5,3 +5,17 @@
 В отличие от предыдущей реализации, выполните эту более простым способом — 
 через сравнение исходной строки с ее перевернутой версией. Если они между собой равны, то это палиндром.
 Для переворота строки воспользуйтесь функцией reverse, которая находится в файле src/Strings.php. */
+
+require_once 'Strings.php';
+function isPalindrome($text){
+    $reverseString = reverse($text);
+    if($text === $reverseString){
+        $result = true;
+    } else {
+        $result = false;
+    };
+
+    return $result;
+};
+
+print_r(isPalindrome('radaredws'));
