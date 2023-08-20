2414ac588219c0941694365dd9a89b189a900d90
magva7 <magva7@gmail.com>
2023-08-20 11:31:42 +0600

    разбираюсь с импортом функций

--- function/import_functions/app/src/Solution.php
+++ function/import_functions/app/src/Solution.php
@@ -1,6 +1,7 @@
 <?php
 
-/*Реализуйте функцию countVowels(), которая принимает строку, считает и возвращает количество гласных букв в ней.
+/*Реализуйте функцию countVowels(), которая принимает строку, считает 
+и возвращает количество гласных букв в ней.
 
 Для проверки, является ли буква гласной, импортируйте и используйте функцию isVowel(). 
 Неймспейс, в котором находится функция определён в файле Symbols.php, сам файл включать не нужно:
@@ -13,15 +14,18 @@ countVowels('One'); // 2
 countVowels('London is the capital of Great Britain'); // 13
  */
 
-namespace App\Solution;
+
+// namespace App\Solution;
 namespace App\Symbols;
+require_once 'Symbols.php';
+// use function App\Symbols\isVowel;
 
 // BEGIN (write your solution here)
-function countVowels($str)
-{
+// function countVowels($str)
+// {
 
-};
-
-countVowels('One'); // 2
-countVowels('London is the capital of Great Britain'); // 13
+// };
+isVowel('o'); // тест импорта неймспейса
+// countVowels('One'); // 2
+// countVowels('London is the capital of Great Britain'); // 13
 // END
