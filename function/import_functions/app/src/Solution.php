d762a652f8cb721099f1c3271c0ddceb4d233ed3
magva7 <magva7@gmail.com>
2023-08-20 23:06:03 +0600

    Разобрался таки с подключением неймспейсов

--- function/import_functions/app/src/Solution.php
+++ function/import_functions/app/src/Solution.php
@@ -14,18 +14,46 @@ countVowels('One'); // 2
 countVowels('London is the capital of Great Britain'); // 13
  */
 
-
-// namespace App\Solution;
-namespace App\Symbols;
-require_once 'Symbols.php';
-// use function App\Symbols\isVowel;
+namespace App\Solution;
+// namespace App\Symbols;
+require_once 'Symbols.php'; // здесь я подключаю файл вручную, а в задании он уже как бы подключен через composer
 
 // BEGIN (write your solution here)
-// function countVowels($str)
-// {
+use function App\Symbols\isVowel;
+function countVowels($str)
+{
+    $countVowelChar = 0; //количество гласных букв
 
-// };
-isVowel('o'); // тест импорта неймспейса
-// countVowels('One'); // 2
-// countVowels('London is the capital of Great Britain'); // 13
+    //пробегаемся по всем символам в переданной строке
+    $i = 0;
+    while($i < mb_strlen($str)){
+        $currentChar = mb_substr($str, $i, 1); // Получаем текущий символ
+        // print_r($char); // печатаем текущий символ для теста
+        $currentCharVowel = isVowel($currentChar); // передаем текущий символ в функцию, которая проверяет, гласная это буква или нет и возвращает 1 или 0
+        if($currentCharVowel){ // если вернулась 1, т.е. текущий символ это гласная буква
+            $countVowelChar = $countVowelChar + 1;  // увеличиваем счетчик гласных букв на 1
+        }
+        $i = $i + 1;
+    }
+    $result = $countVowelChar;
+    return $result;
+}; 
 // END
+// BEGIN
+// use function App\Symbols\isVowel;
+
+// function countVowels($text)
+// {
+//     $len = strlen($text);
+//     $result = 0;
+
+//     for ($i = 0; $i < $len; $i++) {
+//         if (isVowel($text[$i])) {
+//             $result += 1;
+//         }
+//     }
+
+//     return $result;
+// }
+// END
+print_r(countVowels('London is the capital of Great Britain')); // 13
